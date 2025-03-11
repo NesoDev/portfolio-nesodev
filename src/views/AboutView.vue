483546fe067e73cd19eee40548a15604df03d4ee
Nesodev <ever.avendano@unmsm.edu.pe>
2025-03-10 22:15:12 -0500

    building about view

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -21,7 +21,7 @@
 </template>
 
 <script setup>
-import { ref, onMounted, onUnmounted, watch } from 'vue';
+import { ref, onMounted, onUnmounted } from 'vue';
 
 const pRef = ref(null);
 const bgH = ref(0);
